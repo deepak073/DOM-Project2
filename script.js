3164efc3810b6e5cf1c76bb358531370839d8785
deepak073 <89341816+deepak073@users.noreply.github.com>
2023-10-24 13:53:20 +0000

    close the model popup on press on Escape key

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ overlay.addEventListener('click', closeModal);
 
 
 
+document.addEventListener('keydown',function(e){
+  if (e.key ==='Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+    
+  }
+
+})
+
+
