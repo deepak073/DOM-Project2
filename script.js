5240500a0b0897640f0018fc3251ee02b2c84fb2
deepak073 <89341816+deepak073@users.noreply.github.com>
2023-10-24 13:29:56 +0000

    Working properly after miner fixes

--- script.js
+++ script.js
@@ -2,33 +2,28 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelectorAll('.close-modal');
+const btnCloseModal = document.querySelector ('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 // console.log(btnsOpenModal);
-// const openModal = function () {
-//   // console.log('Butten Clicked');
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-// const closeModal = function () {
-//   // console.log('Butten Clicked');
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
 const openModal = function () {
+  // console.log('Butten Clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
 const closeModal = function () {
+  // console.log('Butten Clicked');
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+
+
